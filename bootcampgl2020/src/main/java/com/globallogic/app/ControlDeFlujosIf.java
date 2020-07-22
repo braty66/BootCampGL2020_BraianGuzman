4984abed8e74b41375779faf62895bd952d720aa
braty66 <brian_user@hotmail.com>
2020-07-22 18:09:23 -0300

    Ejercicio 4.2 finalizado de manera satisfactoria

--- bootcampgl2020/src/main/java/com/globallogic/app/ControlDeFlujosIf.java
+++ bootcampgl2020/src/main/java/com/globallogic/app/ControlDeFlujosIf.java
@@ -13,6 +13,29 @@ public class ControlDeFlujosIf {
 		}else {
 			System.out.println("La suma de los numeros es mayor a 10");
 		}
+		
+		String letra = "A";
+		
+		
+		if (letra.equals(letra.toUpperCase())) {
+			
+			System.out.println("Es una letra Mayuscula");
+			
+		}else {
+			System.out.println("Es una letra Minuscula");
+			
+		}
+		String letra2 = "b";
+         
+		if (letra2.equals(letra2.toUpperCase())) {
+			
+			System.out.println("Es una letra Mayuscula");
+			
+		}else {
+			System.out.println("Es una letra Minuscula");
+			
+		}
+		
 	}
 
 }
