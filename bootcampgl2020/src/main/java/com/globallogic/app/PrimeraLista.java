26c126ed60a5f4f248ac5a875e0455dfa2ffea05
braty66 <brian_user@hotmail.com>
2020-07-22 17:31:49 -0300

    Solucionado lo del ejercicio 3.3 mas eficiente cambie el array por un list

--- bootcampgl2020/src/main/java/com/globallogic/app/PrimeraLista.java
+++ bootcampgl2020/src/main/java/com/globallogic/app/PrimeraLista.java
@@ -1,19 +1,34 @@
 package com.globallogic.app;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrimeraLista {
 
 	public static void main(String[] args) {
+	
+		List<String> letters = new ArrayList<String>();
 		
-		String[] letters= {"X","A","B","C"};
+		letters.add("A");
+		letters.add("B");
+		letters.add("C");
 		
-		for (int i= 0;  i<letters.length ; i++) {
-			System.out.println(letters[i]);
+		System.out.println(letters + " " + letters.size());
+		
+		letters.add(0, "X");
+		
+		System.out.println(letters + " " + letters.size());
+		
+		//String[] letters= {"X","A","B","C"};
+		
+		//for (int i= 0;  i<letters.length ; i++) {
+			//System.out.println(letters[i]);
 			
 		}
 		
 		
-		System.out.println(letters.length);
+		//System.out.println(letters.length);
 
 	}
 
-}
+//}
