07fe00896301a5409cc7495d285049b289d19357
braty66 <brian_user@hotmail.com>
2020-07-22 17:40:24 -0300

    finalizado el ejercicio 4.1

---
+++ bootcampgl2020/src/main/java/com/globallogic/app/ControlDeFlujosIf.java
@@ -0,0 +1,18 @@
+package com.globallogic.app;
+
+public class ControlDeFlujosIf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 3;
+		int b = 10;
+		
+		int suma = a + b;
+		if(suma <= 10) {
+			System.out.println("La suma de los numeros es :" + " " + suma );
+		}else {
+			System.out.println("La suma de los numeros es mayor a 10");
+		}
+	}
+
+}
